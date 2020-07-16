b04e74e5b0b2f7d469771883c834fde2fbf47369
Daniel Ferreira <bnmvco@gmail.com>
2020-07-16 20:18:44 -0300

    add merge_with_sections
    Add a script to merge the output of match_geocoding.R and generate a
    final output.

---
+++ merge_with_sections.R
@@ -0,0 +1,38 @@
+# merge_with_sections.R
+# (c) 2020 CincoNoveSeis Jornalismo Ltda.
+#
+# This program is licensed under the GNU General Public License, version 3.
+# See the LICENSE file for details.
+
+library(readr)
+library(readxl)
+library(dplyr)
+library(stringi)
+library(stringr)
+
+source('tse_file_reader.R')
+source('match_shared.R')
+
+all_years = bind_rows(
+    open_2012(),
+    open_2014(),
+    open_2016(),
+    open_2018()
+) %>%
+    mutate(norm_local = normalize_place(local)) %>%
+    select(ano, codigo_ibge, norm_local, uf, zona, secao) %>%
+    mutate(ID_n = row_number())
+
+addr_current = read_csv('ADDR_CURRENT.csv') %>%
+    mutate(norm_local = normalize_place(local)) %>%
+    select(-ID, -uf, -cidade, -bairro, -local)
+
+geocoded_secoes_orig = inner_join(all_years, addr_current, by = c(
+    'codigo_ibge' = 'codigo_ibge',
+    'norm_local' = 'norm_local'
+)) %>%
+    group_by(ID_n) %>%
+    filter(n() == 1) %>%
+    ungroup() %>%
+    select(-norm_local, -ID_n, -endereco)
+
